fc764f123221ca0b4b2a0ca0035ace458f190c95
Sirajus Salekin <ssalek14@earlham.edu>
2017-11-04 23:11:20 -0400

    uses system time to get a random int

--- randomNonIO.hs
+++ randomNonIO.hs
@@ -1,11 +1,18 @@
 import System.Random
+import Data.Time.Clock.POSIX
+import System.IO.Unsafe
+
+-- This is using unsafePerformIO, since we will be using
+-- the random shuffle for using things that might not 
+-- involve IO. This is not the recommended way, but it gives
+-- us a random number to use.
+integralTime = round (unsafePerformIO getPOSIXTime) :: Int
 
 getRandom :: Int -> Int
-getRandom n = 1234435 `mod` n
+getRandom n = integralTime `mod` n
 
 
 shuffle :: [a] -> [a]
---shuffle [] = []
 shuffle xs 
     | length xs < 2 = xs
     | otherwise = do
